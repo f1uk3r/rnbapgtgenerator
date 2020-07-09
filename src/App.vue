531eeb9e565a3cf27101e79aaebf95b1a461d8d6
f1uk3r <agarwal.umang4321@gmail.com>
2020-07-10 00:35:48 +0530

    logo css id assigned

--- src/App.vue
+++ src/App.vue
@@ -25,4 +25,154 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+#atl {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) 0 0;
+}
+#bos {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) 0 40;
+}
+#bkl {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) 0 80;
+}
+#cha {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) 0 120;
+}
+#chi {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) 0 160;
+}
+#sac {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) 0 200;
+}
+#cle {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -40 0;
+}
+#dal {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -40 40;
+}
+#den {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -40 80;
+}
+#det {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -40 120;
+}
+#gsw {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -40 160;
+}
+#sas {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -40 200;
+}
+#hou {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -80 0;
+}
+#ind {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -80 40;
+}
+#lac {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -80 80;
+}
+#lal {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -80 120;
+}
+#mem {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -80 160;
+}
+#tor {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -80 200;
+}
+#mia {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -120 0;
+}
+#mil {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -120 40;
+}
+#min {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -120 80;
+}
+#nop {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -120 120;
+}
+#nyk {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -120 160;
+}
+#uta {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -120 200;
+}
+#okc {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -160 0;
+}
+#orl {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -160 40;
+}
+#phi {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -160 80;
+}
+#phx {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -160 120;
+}
+#por {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -160 160;
+}
+#was {
+  width: 40px;
+  height: 40px;
+  background: url(../assets/teams-logo.png) -160 200;
+}
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -11,210 +11,240 @@ const store = new Vuex.Store({
         "01",
         "atlanta-hawks-",
         "http://np.reddit.com/r/atlantahawks",
-        "1610612737"
+        "1610612737",
+        "atl"
       ],
       "BKN": [
         "Brooklyn Nets",
         "02",
         "boston-celtics-",
         "http://np.reddit.com/r/bostonceltics",
-        "1610612738"
+        "1610612738",
+        "bkn"
       ],
       "BOS": [
         "Boston Celtics",
         "17",
         "brooklyn-nets-",
         "http://np.reddit.com/r/gonets",
-        "1610612751"
+        "1610612751",
+        "bos"
       ],
       "CHA": [
         "Charlotte Hornets",
         "30",
         "charlotte-hornets-",
         "http://np.reddit.com/r/charlottehornets",
-        "1610612766"
+        "1610612766",
+        "cha"
       ],
       "CHI": [
         "Chicago Bulls",
         "04",
         "chicago-bulls-",
         "http://np.reddit.com/r/chicagobulls",
-        "1610612741"
+        "1610612741",
+        "chi"
       ],
       "CLE": [
         "Cleveland Cavaliers",
         "05",
         "cleveland-cavaliers-",
         "http://np.reddit.com/r/clevelandcavs",
-        "1610612739"
+        "1610612739",
+        "cle"
       ],
       "DAL": [
         "Dallas Mavericks",
         "06",
         "dallas-mavericks-",
         "http://np.reddit.com/r/mavericks",
-        "1610612742"
+        "1610612742",
+        "dal"
       ],
       "DEN": [
         "Denver Nuggets",
         "07",
         "denver-nuggets-",
         "http://np.reddit.com/r/denvernuggets",
-        "1610612743"
+        "1610612743",
+        "den"
       ],
       "DET": [
         "Detroit Pistons",
         "08",
         "detroit-pistons-",
         "http://np.reddit.com/r/detroitpistons",
-        "1610612765"
+        "1610612765",
+        "det"
       ],
       "GSW": [
         "Golden State Warriors",
         "09",
         "golden-state-warriors-",
         "http://np.reddit.com/r/warriors",
-        "1610612744"
+        "1610612744",
+        "gsw"
       ],
       "HOU": [
         "Houston Rockets",
         "10",
         "houston-rockets-",
         "http://np.reddit.com/r/rockets",
-        "1610612745"
+        "1610612745",
+        "hou"
       ],
       "IND": [
         "Indiana Pacers",
         "11",
         "indiana-pacers-",
         "http://np.reddit.com/r/pacers",
-        "1610612754"
+        "1610612754",
+        "ind"
       ],
       "LAC": [
         "Los Angeles Clippers",
         "12",
         "los-angeles-clippers-",
         "http://np.reddit.com/r/laclippers",
-        "1610612746"
+        "1610612746",
+        "lac"
       ],
       "LAL": [
         "Los Angeles Lakers",
         "13",
         "los-angeles-lakers-",
         "http://np.reddit.com/r/lakers",
-        "1610612747"
+        "1610612747",
+        "lal"
       ],
       "MEM": [
         "Memphis Grizzlies",
         "29",
         "memphis-grizzlies-",
         "http://np.reddit.com/r/memphisgrizzlies",
-        "1610612763"
+        "1610612763",
+        "mem"
       ],
       "MIA": [
         "Miami Heat",
         "14",
         "miami-heat-",
         "http://np.reddit.com/r/heat",
-        "1610612748"
+        "1610612748",
+        "mia"
       ],
       "MIL": [
         "Milwaukee Bucks",
         "15",
         "milwaukee-bucks-",
         "http://np.reddit.com/r/mkebucks",
-        "1610612749"
+        "1610612749",
+        "mil"
       ],
       "MIN": [
         "Minnesota Timberwolves",
         "16",
         "minnesota-timberwolves-",
         "http://np.reddit.com/r/timberwolves",
-        "1610612750"
+        "1610612750",
+        "min"
       ],
       "NOP": [
         "New Orleans Pelicans",
         "03",
         "new-orleans-pelicans-",
         "http://np.reddit.com/r/nolapelicans",
-        "1610612740"
+        "1610612740",
+        "nop"
       ],
       "NYK": [
         "New York Knicks",
         "18",
         "new-york-knicks-",
         "http://np.reddit.com/r/nyknicks",
-        "1610612752"
+        "1610612752",
+        "nyk"
       ],
       "OKC": [
         "Oklahoma City Thunder",
         "25",
         "oklahoma-city-thunder-",
         "http://np.reddit.com/r/thunder",
-        "1610612760"
+        "1610612760",
+        "okc"
       ],
       "ORL": [
         "Orlando Magic",
         "19",
         "orlando-magic-",
         "http://np.reddit.com/r/orlandomagic",
-        "1610612753"
+        "1610612753",
+        "orl"
       ],
       "PHI": [
         "Philadelphia 76ers",
         "20",
         "philadelphia-76ers-",
         "http://np.reddit.com/r/sixers",
-        "1610612755"
+        "1610612755",
+        "phi"
       ],
       "PHX": [
         "Phoenix Suns",
         "21",
         "phoenix-suns-",
         "http://np.reddit.com/r/suns",
-        "1610612756"
+        "1610612756",
+        "phx"
       ],
       "POR": [
         "Portland Trail Blazers",
         "22",
         "portland-trail-blazers-",
         "http://np.reddit.com/r/ripcity",
-        "1610612757"
+        "1610612757",
+        "por"
       ],
       "SAC": [
         "Sacramento Kings",
         "23",
         "sacramento-kings-",
         "http://np.reddit.com/r/kings",
-        "1610612758"
+        "1610612758",
+        "sac"
       ],
       "SAS": [
         "San Antonio Spurs",
         "24",
         "san-antonio-spurs-",
         "http://np.reddit.com/r/nbaspurs",
-        "1610612759"
+        "1610612759",
+        "sas"
       ],
       "TOR": [
         "Toronto Raptors",
         "28",
         "toronto-raptors-",
         "http://np.reddit.com/r/torontoraptors",
-        "1610612761"
+        "1610612761",
+        "tor"
       ],
       "UTA": [
         "Utah Jazz",
         "26",
         "utah-jazz-",
         "http://np.reddit.com/r/utahjazz",
-        "1610612762"
+        "1610612762",
+        "uta"
       ],
       "WAS": [
         "Washington Wizards",
         "27",
         "washington-wizards-",
         "http://np.reddit.com/r/washingtonwizards",
-        "1610612764"
+        "1610612764",
+        "was"
       ],
     }
   }
