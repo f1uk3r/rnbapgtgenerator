7550598df2d164c91336cf380d422fd6b061f20d
f1uk3r <agarwal.umang4321@gmail.com>
2020-07-10 00:07:09 +0530

    teamData saved in store

--- src/store/store.js
+++ src/store/store.js
@@ -4,9 +4,218 @@ import Vuex from 'vuex'
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-    state: {
-        teamsData: {
-            
-        }
+  state: {
+    teamsData: {
+      "ATL": [
+        "Atlanta Hawks",
+        "01",
+        "atlanta-hawks-",
+        "http://np.reddit.com/r/atlantahawks",
+        "1610612737"
+      ],
+      "BKN": [
+        "Brooklyn Nets",
+        "02",
+        "boston-celtics-",
+        "http://np.reddit.com/r/bostonceltics",
+        "1610612738"
+      ],
+      "BOS": [
+        "Boston Celtics",
+        "17",
+        "brooklyn-nets-",
+        "http://np.reddit.com/r/gonets",
+        "1610612751"
+      ],
+      "CHA": [
+        "Charlotte Hornets",
+        "30",
+        "charlotte-hornets-",
+        "http://np.reddit.com/r/charlottehornets",
+        "1610612766"
+      ],
+      "CHI": [
+        "Chicago Bulls",
+        "04",
+        "chicago-bulls-",
+        "http://np.reddit.com/r/chicagobulls",
+        "1610612741"
+      ],
+      "CLE": [
+        "Cleveland Cavaliers",
+        "05",
+        "cleveland-cavaliers-",
+        "http://np.reddit.com/r/clevelandcavs",
+        "1610612739"
+      ],
+      "DAL": [
+        "Dallas Mavericks",
+        "06",
+        "dallas-mavericks-",
+        "http://np.reddit.com/r/mavericks",
+        "1610612742"
+      ],
+      "DEN": [
+        "Denver Nuggets",
+        "07",
+        "denver-nuggets-",
+        "http://np.reddit.com/r/denvernuggets",
+        "1610612743"
+      ],
+      "DET": [
+        "Detroit Pistons",
+        "08",
+        "detroit-pistons-",
+        "http://np.reddit.com/r/detroitpistons",
+        "1610612765"
+      ],
+      "GSW": [
+        "Golden State Warriors",
+        "09",
+        "golden-state-warriors-",
+        "http://np.reddit.com/r/warriors",
+        "1610612744"
+      ],
+      "HOU": [
+        "Houston Rockets",
+        "10",
+        "houston-rockets-",
+        "http://np.reddit.com/r/rockets",
+        "1610612745"
+      ],
+      "IND": [
+        "Indiana Pacers",
+        "11",
+        "indiana-pacers-",
+        "http://np.reddit.com/r/pacers",
+        "1610612754"
+      ],
+      "LAC": [
+        "Los Angeles Clippers",
+        "12",
+        "los-angeles-clippers-",
+        "http://np.reddit.com/r/laclippers",
+        "1610612746"
+      ],
+      "LAL": [
+        "Los Angeles Lakers",
+        "13",
+        "los-angeles-lakers-",
+        "http://np.reddit.com/r/lakers",
+        "1610612747"
+      ],
+      "MEM": [
+        "Memphis Grizzlies",
+        "29",
+        "memphis-grizzlies-",
+        "http://np.reddit.com/r/memphisgrizzlies",
+        "1610612763"
+      ],
+      "MIA": [
+        "Miami Heat",
+        "14",
+        "miami-heat-",
+        "http://np.reddit.com/r/heat",
+        "1610612748"
+      ],
+      "MIL": [
+        "Milwaukee Bucks",
+        "15",
+        "milwaukee-bucks-",
+        "http://np.reddit.com/r/mkebucks",
+        "1610612749"
+      ],
+      "MIN": [
+        "Minnesota Timberwolves",
+        "16",
+        "minnesota-timberwolves-",
+        "http://np.reddit.com/r/timberwolves",
+        "1610612750"
+      ],
+      "NOP": [
+        "New Orleans Pelicans",
+        "03",
+        "new-orleans-pelicans-",
+        "http://np.reddit.com/r/nolapelicans",
+        "1610612740"
+      ],
+      "NYK": [
+        "New York Knicks",
+        "18",
+        "new-york-knicks-",
+        "http://np.reddit.com/r/nyknicks",
+        "1610612752"
+      ],
+      "OKC": [
+        "Oklahoma City Thunder",
+        "25",
+        "oklahoma-city-thunder-",
+        "http://np.reddit.com/r/thunder",
+        "1610612760"
+      ],
+      "ORL": [
+        "Orlando Magic",
+        "19",
+        "orlando-magic-",
+        "http://np.reddit.com/r/orlandomagic",
+        "1610612753"
+      ],
+      "PHI": [
+        "Philadelphia 76ers",
+        "20",
+        "philadelphia-76ers-",
+        "http://np.reddit.com/r/sixers",
+        "1610612755"
+      ],
+      "PHX": [
+        "Phoenix Suns",
+        "21",
+        "phoenix-suns-",
+        "http://np.reddit.com/r/suns",
+        "1610612756"
+      ],
+      "POR": [
+        "Portland Trail Blazers",
+        "22",
+        "portland-trail-blazers-",
+        "http://np.reddit.com/r/ripcity",
+        "1610612757"
+      ],
+      "SAC": [
+        "Sacramento Kings",
+        "23",
+        "sacramento-kings-",
+        "http://np.reddit.com/r/kings",
+        "1610612758"
+      ],
+      "SAS": [
+        "San Antonio Spurs",
+        "24",
+        "san-antonio-spurs-",
+        "http://np.reddit.com/r/nbaspurs",
+        "1610612759"
+      ],
+      "TOR": [
+        "Toronto Raptors",
+        "28",
+        "toronto-raptors-",
+        "http://np.reddit.com/r/torontoraptors",
+        "1610612761"
+      ],
+      "UTA": [
+        "Utah Jazz",
+        "26",
+        "utah-jazz-",
+        "http://np.reddit.com/r/utahjazz",
+        "1610612762"
+      ],
+      "WAS": [
+        "Washington Wizards",
+        "27",
+        "washington-wizards-",
+        "http://np.reddit.com/r/washingtonwizards",
+        "1610612764"
+      ],
     }
+  }
 })
