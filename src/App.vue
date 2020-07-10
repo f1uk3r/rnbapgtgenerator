91e0b4fb7121452018f77cb0f4d9f38ce8bc88fc
f1uk3r <agarwal.umang4321@gmail.com>
2020-07-10 23:20:22 +0530

    updated positions in spritesheet

--- src/App.vue
+++ src/App.vue
@@ -32,146 +32,146 @@ export default {
 #bos {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) 0 40;
+  background: url(./assets/teams-logo.png) 200px 0;
 }
-#bkl {
+#bkn {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) 0 80;
+  background: url(./assets/teams-logo.png) 160px 0;
 }
 #cha {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) 0 120;
+  background: url(./assets/teams-logo.png) 120px 0;
 }
 #chi {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) 0 160;
+  background: url(./assets/teams-logo.png) 80px 0;
 }
 #sac {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) 0 200;
+  background: url(./assets/teams-logo.png) 40px 0;
 }
 #cle {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -40 0;
+  background: url(./assets/teams-logo.png) 0 160px;
 }
 #dal {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -40 40;
+  background: url(./assets/teams-logo.png) 200px 160px;
 }
 #den {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -40 80;
+  background: url(./assets/teams-logo.png) 160px 160px;
 }
 #det {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -40 120;
+  background: url(./assets/teams-logo.png) 120px 160px;
 }
 #gsw {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -40 160;
+  background: url(./assets/teams-logo.png) 80px 160px;
 }
 #sas {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -40 200;
+  background: url(./assets/teams-logo.png) 40px 160px;
 }
 #hou {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -80 0;
+  background: url(./assets/teams-logo.png) 0 120px;
 }
 #ind {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -80 40;
+  background: url(./assets/teams-logo.png) 200px 120px;
 }
 #lac {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -80 80;
+  background: url(./assets/teams-logo.png) 160px 120px;
 }
 #lal {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -80 120;
+  background: url(./assets/teams-logo.png) 120px 120px;
 }
 #mem {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -80 160;
+  background: url(./assets/teams-logo.png) 80px 120px;
 }
 #tor {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -80 200;
+  background: url(./assets/teams-logo.png) 40px 120px;
 }
 #mia {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -120 0;
+  background: url(./assets/teams-logo.png) 0 80px;
 }
 #mil {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -120 40;
+  background: url(./assets/teams-logo.png) 200px 80px;
 }
 #min {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -120 80;
+  background: url(./assets/teams-logo.png) 160px 80px;
 }
 #nop {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -120 120;
+  background: url(./assets/teams-logo.png) 120px 80px;
 }
 #nyk {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -120 160;
+  background: url(./assets/teams-logo.png) 80px 80px;
 }
 #uta {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -120 200;
+  background: url(./assets/teams-logo.png) 40px 80px;
 }
 #okc {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -160 0;
+  background: url(./assets/teams-logo.png) 0 40px;
 }
 #orl {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -160 40;
+  background: url(./assets/teams-logo.png) 200px 40px;
 }
 #phi {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -160 80;
+  background: url(./assets/teams-logo.png) 160px 40px;
 }
 #phx {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -160 120;
+  background: url(./assets/teams-logo.png) 120px 40px;
 }
 #por {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -160 160;
+  background: url(./assets/teams-logo.png) 80px 40px;
 }
 #was {
   width: 40px;
   height: 40px;
-  background: url(./assets/teams-logo.png) -160 200;
+  background: url(./assets/teams-logo.png) 40px 40px;
 }
 </style>
