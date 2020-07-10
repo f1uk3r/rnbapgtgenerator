646d13589161e357b457412de21c8760f2c5ec6e
f1uk3r <agarwal.umang4321@gmail.com>
2020-07-10 16:11:42 +0530

    added axios plugin globally

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.use(Buefy)
+Vue.use({
+  install (Vue) {
+    Vue.prototype.$api = axios.create({
+      baseURL: 'http://data.nba.net/prod/v1/'
+    })
+  }
+})
 
 new Vue({
   render: h => h(App),
