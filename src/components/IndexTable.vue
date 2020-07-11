e82ca9908e5161463d5819da01d1e82f4e21ba3d
f1uk3r <agarwal.umang4321@gmail.com>
2020-07-11 13:15:25 +0530

    Index Table done

--- src/components/IndexTable.vue
+++ src/components/IndexTable.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <section>
+    <b-table :data="calculateDataToDisplay" :columns="columns" sticky-header></b-table>
+  </section>
 </template>
 
 <script>
@@ -7,6 +9,54 @@ export default {
   name: 'IndexTable',
   props: {
     gamesData: Array
+  },
+  data () {
+    return {
+      columns: [
+        {
+          field: 'start_time',
+          label: 'Start Time (ET)'
+        },
+        {
+          field: 'home_team',
+          label: 'Home Team',
+          searchable: true
+        },
+        {
+          field: 'away_team',
+          label: 'Away Team',
+          searchable: true
+        },
+        {
+          field: 'score',
+          label: 'Score'
+        },
+        {
+          field: 'clock',
+          label: 'Clock'
+        }
+      ]
+    }
+  },
+  computed: {
+    calculateDataToDisplay () {
+      let listToReturn = []
+      for (let i = 0; i<this.gamesData.length; i++) {
+        let dataToReturn = {
+          'start_time': this.gamesData[i].startTimeEastern,
+          'home_team': this.$store.getters.teamsData[this.gamesData[i].hTeam.triCode][0],
+          'away_team': this.$store.getters.teamsData[this.gamesData[i].vTeam.triCode][0],
+          'score': this.gamesData[i].hTeam.score + " - " + this.gamesData[i].vTeam.score,
+          'clock': (this.gamesData[i].period.current===1 && this.gamesData[i].clock==='')
+            ? "PRE-GAME"
+            : (this.gamesData[i].period.current>=4 && (this.gamesData[i].clock===''||this.gamesData[i].clock==='0.0') && (this.gamesData[i].hTeam.score !== this.gamesData[i].vTeam.score))
+            ? "FINAL"
+            : this.gamesData[i].clock
+        }
+        listToReturn.push(dataToReturn)
+      }
+      return listToReturn
+    }
   }
 }
 </script>
