77b90217398f8714793c8b859d898df145b94b22
f1uk3r <agarwal.umang4321@gmail.com>
2020-07-11 15:20:06 +0530

    game components initialised

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,9 +1,31 @@
 <template>
-  
+  <div>
+    <GameBar></GameBar>
+    <GameTable></GameTable>
+    <GameTableReddit></GameTableReddit>
+  </div>
 </template>
 
 <script>
+import GameBar from './GameBar.vue'
+import GameTable from './GameTable.vue'
+import GameTableReddit from './GameTableReddit.vue'
+
 export default {
-  name: 'Game'
+  name: 'Game',
+  components: {
+    GameBar,
+    GameTable,
+    GameTableReddit
+  },
+  data () {
+    return {
+      currentGameData: null,
+      scoreboardData: null,
+
+      url: "http://data.nba.net/prod/v1/" + this.$store.getters.dateToday + '/scoreboard.json'
+    }
+  }
+
 }
 </script>
---
+++ src/components/GameTable.vue
@@ -0,0 +1,9 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'GameTable'
+}
+</script>
